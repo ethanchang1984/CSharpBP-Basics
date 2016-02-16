773c0886ef0886d41f74715b64a2834bccc2235d
Michael <zuominzhang@yeah.net>
2016-02-16 14:25:13 +0800

    class module

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Honda";
+            currentProduct.ProductId = 2346;
+            currentProduct.Description = "Nice car from Japan";
+            var expected = "Hello Honda (2346): Nice car from Japan";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_ParameterizedConstructor_Test()
+        {
+            //Arrange
+            var currentProduct = new Product("Honda", 2346, "Nice car from Japan");
+            var expected = "Hello Honda (2346): Nice car from Japan";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
